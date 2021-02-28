5d687e3f4929861e3dece1e478517f037dfc4f79
Igor0006 <77965867+Igor0006@users.noreply.github.com>
2021-02-28 11:59:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import sqlite3
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QLineEdit
+from PyQt5 import uic
+from design import Ui_Form as Design
+
+
+class MyWidget(QWidget, Design):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.titles = None
+        self.update_result()
+
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("""Select * from coffee""").fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
